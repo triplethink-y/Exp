2c1c40701903930857572bd61aa6c004b335f19a
Yoyo Kiwi <dduangduang6@gmail.com>
2020-08-14 13:00:37 -0400

    how likely we're to encounter an elderly snake

--- snake.py
+++ snake.py
@@ -1,2 +1,19 @@
-print("This is a snake file")
-snake_age = 30
+from random import randint
+print("Elderly snake investigator.\n")
+#snake_age = 0
+o_count = 0
+y_count = 0
+def is_old(age):
+    if age>=30:
+        return True
+    return False
+
+times = int(input("How many snakes would you like to investigate today? "))
+for i in range(times):
+    age = randint(1, 60)
+    if is_old(age):
+        o_count +=1
+    else:
+        y_count+=1
+
+print("out of", times, "snakes,", (o_count/times)*100, "% were old.")
